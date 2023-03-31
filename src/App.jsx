eddbec8a3452fbddca5e8d5422e3f8fd1fa2be83
ikkedaniel <3222250-ikkedaniel@users.noreply.gitlab.com>
2023-03-31 23:18:49 +0900

    add fakerjs data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import './App.sass'
+import { faker } from '@faker-js/faker'
 
 function Card() {
   return (
     <article className="relative flex flex-col max-w-xs p-6 -ml-32 duration-200 ease-in-out group aspect-card bg-neutral-700 card rounded-2xl first:ml-0 hover:-translate-y-4">
       <header>
-        <h1>Lorem, ipsum.</h1>
-        <p className="group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-purple-400 group-hover:to-pink-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
+        <p>{faker.date.recent().toDateString()}</p>
+        <p className="mt-4 text-2xl group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-purple-400 group-hover:to-pink-600">
+          {faker.lorem.sentence()}
         </p>
       </header>
 
       <div className="relative flex mt-auto">
-        <img className="w-16 h-16 mr-4 rounded-full grayscale" src="https://picsum.photos/400/400" alt="avatar" />
+        <img className="w-12 h-12 mt-auto mr-4 rounded-full" src={faker.image.image(50, 50, true)} alt="avatar" />
 
         <span>
-          <strong>Author</strong>
-          <p>John Doe</p>
+          <strong className="text-neutral-400">{faker.name.jobType()}</strong>
+          <p>{faker.name.firstName()}</p>
         </span>
       </div>
     </article>
@@ -25,13 +26,7 @@ function Card() {
 function App() {
   return (
     <>
-      <section className="flex p-12 overflow-hidden">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </section>
+      <section className="flex p-12 overflow-hidden">{Array(5).fill(<Card />)}</section>
     </>
   )
 }
