0e5b1fffdd98443936b28cd65178f9b15d4170c5
ikkedaniel <3222250-ikkedaniel@users.noreply.gitlab.com>
2023-04-01 13:29:40 +0900

    removed awkward tailwind css

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,16 +9,20 @@ export function Card() {
   let name = faker.name.firstName()
 
   return (
-    <article className="relative flex flex-col p-6 -ml-32 duration-200 ease-in-out w-80 group aspect-card bg-neutral-700 rounded-2xl first:ml-0 hover:-translate-y-4">
+    <article className="relative flex flex-col p-6 duration-200 ease-in-out w-80 group aspect-card bg-neutral-700 rounded-2xl hover:-translate-y-4">
       <header>
         <p>{date}</p>
-        <p className="mt-4 text-2xl group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-purple-400 group-hover:to-pink-600">
+        <p className="mt-4 text-xl group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-purple-400 group-hover:to-pink-600">
           {description}
         </p>
       </header>
 
       <div className="relative flex mt-auto">
-        <img className="w-12 h-12 mt-auto mr-4 rounded-full" src={avatar} alt="avatar" />
+        <img
+          className="w-12 h-12 mt-auto mr-4 rounded-full grayscale group-hover:grayscale-0"
+          src={avatar}
+          alt="avatar"
+        />
 
         <span>
           <strong className="text-neutral-400">{jobType}</strong>
